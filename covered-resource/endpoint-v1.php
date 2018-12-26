1110d95933fad6e81f41cf70a525964dc9253abe
Alistair Mann <al+tfl@pectw.net>
2018-12-26 13:51:07 +0000

    Hello world for covered-resource

---
+++ covered-resource/endpoint-v1.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  This endpoint will initially live at http://berewic.pectw.net
+
+  The output will reflect the presence or absence in the inbound
+  connections headers of berewic information.
+
+  This endpoint ultimately grows into /Bob's server/: he hosts content here
+  which he wishes to have protected using berewic bonds.
+ */
+//
+// globals
+error_reporting(E_STRICT);
+
+function main() {
+  echo "Hello, world!\n";
+}
+
+main();
+?>
