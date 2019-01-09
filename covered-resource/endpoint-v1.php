00a64b538bda63500935853681d4da7709ab26c4
Alistair Mann <al+tfl@pectw.net>
2019-01-09 03:30:13 +0000

    Add more RESTful-like /proposal part to the URLs used for BTAs Add berewic-bonded: true|false header

--- covered-resource/endpoint-v1.php
+++ covered-resource/endpoint-v1.php
@@ -7,16 +7,17 @@
 
   This endpoint ultimately grows into /Bob's server/: he hosts content here
   which he wishes to have protected using berewic bonds.
- */
+*/
 //
 // globals
 error_reporting(E_ALL);
 
 // fakes! don't use
-define('BEREWIC_BTA_1', 'https://z4r56iqh3gnhj67bn.onion:9090/35cb904aced147bd1230965501af4306ac5204db/64be35a9db5e5f09f40a058b9f27c5ed112ea49c');
-define('BEREWIC_BTA_2', 'https://berewic.mpsvr.com:8080/4025061200627151c0c2b7b80d7af47b3b5c8bd2/e8ebaa9cb957844658dd0bcea2aeae6ffb1e2349');
+define('BEREWIC_BTA_1', 'https://z4r56iqh3gnhj67bn.onion:9090/proposal/35cb904aced147bd1230965501af4306ac5204db/64be35a9db5e5f09f40a058b9f27c5ed112ea49c');
+define('BEREWIC_BTA_2', 'https://berewic.mpsvr.com:8080/proposal/4025061200627151c0c2b7b80d7af47b3b5c8bd2/e8ebaa9cb957844658dd0bcea2aeae6ffb1e2349');
 define('BEREWIC_SECRET', 'thisisasecret');
 
+define('CONST_HEADER_BEREWIC_BONDED', 'berewic-bonded');
 define('CONST_HEADER_KEY_MARKER', 'berewic-');
 define('CONST_HEADER_KEY_MARKER_SZ', strlen(CONST_HEADER_KEY_MARKER));
 define('CONST_HEADER_KEY_MAX_SZ', 32);
@@ -90,7 +91,7 @@ class BerewicHeaders {
 			case ERR_HEADER_KEY_UNKNOWN:
 				echo ERR_HEADER_KEY_UNKNOWN_MSG . "\n";
 				break;
-			case ERR_HEADER_TEMP_RATE_UNKNOWN;
+			case ERR_HEADER_TEMP_RATE_UNKNOWN:
 				echo ERR_HEADER_TEMP_RATE_UNKNOWN_MSG . "\n";
 				break;
 			default:
@@ -247,10 +248,12 @@ class OutHeaders extends BerewicHeaders {
 function main($headers) {
 	$inHeaders = new InHeaders($headers);
 	if ($inHeaders->getShenanigans() === true) {
+		header(CONST_HEADER_BEREWIC_BONDED . ": false");
 		echo "Bonding headers seen but shenanigans detected:\n";
 		$inHeaders->printShenanigans();
 
 	} elseif ($inHeaders->getHeadersSeen() === 0) {
+		header(CONST_HEADER_BEREWIC_BONDED . ": false");
 		$id = '';
 		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
 			$id .= $_SERVER['HTTP_X_FORWARDED_FOR'];
@@ -274,6 +277,7 @@ function main($headers) {
 		}
 
 	} else {
+		header(CONST_HEADER_BEREWIC_BONDED . ": true");
 		echo "Congratulations! This connection is bonded\n";
 	}
 
